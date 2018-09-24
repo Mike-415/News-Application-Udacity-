7c9bc362f255f9c57b2ea34f40d67def6c72167a
Mike-415 <mgonz160@mail.ccsf.edu>
2018-09-24 11:39:24 -0700

    implemented LoaderCallbacks in MainActivity.  Had to use deprecated version, but it works

--- app/src/main/java/com/example/android/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/android/newsapplication/MainActivity.java
@@ -3,21 +3,17 @@ package com.example.android.newsapplication;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.List;
 
-//TODO: Find the most recent type of LoaderManager
+
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>{
     private static final String TAG = "MainActivity";
-    private static final String GUARDIAN_QUERY_URL = "http://content.guardianapis.com/world?show-most-viewed=true&page-size=1&show-fields=headline,thumbnail&show-tags=contributor&api-key=ef581bc8-daf3-4727-9d6d-67623f4f80d9";
+    private static final String GUARDIAN_QUERY_URL = "http://content.guardianapis.com/world?show-most-viewed=true&show-fields=headline,thumbnail&show-tags=contributor&api-key=ef581bc8-daf3-4727-9d6d-67623f4f80d9";
     private static final int NEWS_LOADER_ID = 0;
 
     @Override
@@ -28,25 +24,26 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo network = connMgr.getActiveNetworkInfo();
         if(network != null && network.isConnected()){
-
+            getLoaderManager().initLoader(NEWS_LOADER_ID, null, MainActivity.this);
         }
 
 
     }
 
-    @NonNull
     @Override
-    public Loader<List<News>> onCreateLoader(int i, @Nullable Bundle bundle) {
+    public android.content.Loader<List<News>> onCreateLoader(int id, Bundle args) {
         return new NewsLoader(MainActivity.this, GUARDIAN_QUERY_URL);
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<List<News>> loader, List<News> newsList) {
+    public void onLoadFinished(android.content.Loader<List<News>> loader, List<News> newsList) {
         Log.d(TAG, "onLoadFinished: GOT THE LIST!!!! LIST SIZE: "+newsList.size());
     }
 
     @Override
-    public void onLoaderReset(@NonNull Loader loader) {
+    public void onLoaderReset(android.content.Loader<List<News>> loader) {
 
     }
 }
+
+
--- app/src/main/java/com/example/android/newsapplication/NewsLoader.java
+++ app/src/main/java/com/example/android/newsapplication/NewsLoader.java
@@ -1,9 +1,11 @@
 package com.example.android.newsapplication;
 
 import android.content.Context;
+
+import android.content.AsyncTaskLoader;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.AsyncTaskLoader;
 import android.text.TextUtils;
 
 import java.util.List;
@@ -12,7 +14,7 @@ public class NewsLoader extends AsyncTaskLoader<List<News>>{
     private static final String TAG = "NewsLoader";
     private String mUrl;
 
-    public NewsLoader(@NonNull Context context, String url) {
+    public NewsLoader(Context context, String url) {
         super(context);
         mUrl = url;
     }
@@ -22,7 +24,6 @@ public class NewsLoader extends AsyncTaskLoader<List<News>>{
         forceLoad();
     }
 
-    @Nullable
     @Override
     public List<News> loadInBackground() {
         if(TextUtils.isEmpty(mUrl)){
