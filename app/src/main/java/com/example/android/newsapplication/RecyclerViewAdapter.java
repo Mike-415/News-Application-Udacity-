8fba87b678f9bc29d7af5234d9b8ba03a181bbd7
Mike-415 <mgonz160@mail.ccsf.edu>
2018-09-24 17:09:34 -0700

    Added Glide and ButterKnife and completed most of the RecyclerViewAdapter

--- app/src/main/java/com/example/android/newsapplication/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/newsapplication/RecyclerViewAdapter.java
@@ -1,99 +1,74 @@
 package com.example.android.newsapplication;
 
-import android.content.Intent;
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class RecyclerViewAdapter {
-    public class ViewHolder extends RecyclerView.ViewHolder {
+import java.util.List;
 
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+    private List<News> mNewsList;
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, List<News> newsList){
+        mContext = context;
+        mNewsList = newsList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        News news = mNewsList.get(i);
+        //We can use Picasso and Butterknife, just as long as it has nothing to do with networking
+        //TODO: Load image using Glide or Picasso
+        viewHolder.thumbnail.setImageResource(R.drawable.ic_launcher_foreground);
+        viewHolder.sectionName.setText(news.getSection());
+        viewHolder.headline.setText(news.getTitle());
+        viewHolder.author.setText(news.getAuthor());
+        viewHolder.dateAndTime.setText(news.getDate());
+        viewHolder.parentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Create another field in the News class for the Guardian URL
+                //TODO: Create an intent for the Guardian URL
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNewsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView parentView;
+        private ImageView thumbnail;
+        private TextView sectionName, headline, author, dateAndTime;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            parentView = itemView.findViewById(R.id.listItemCardView);
+            thumbnail = itemView.findViewById(R.id.thumbnail);
+            sectionName = itemView.findViewById(R.id.sectionName);
+            headline = itemView.findViewById(R.id.headline);
+            author = itemView.findViewById(R.id.author);
+            dateAndTime = itemView.findViewById(R.id.dateAndTime);
+
+
         }
     }
 }
 
-/*
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private static final String TAG = "RecyclerViewAdapter";
-    private List<Site> mSites;
-    private Context mContext;
-
-
-    public RecyclerViewAdapter(List<Site> sites, Context context) {
-        this.mSites = sites;
-        this.mContext = context;
-    }
-
-    /**
-     * This inner class contains all the data that is within each list item
-     */
-//public class ViewHolder extends RecyclerView.ViewHolder{
-//    @BindView(R.id.parent_layout)
-//    RelativeLayout mParentLayout;
-//    @BindView(R.id.list_item_image_view)
-//    ImageView mImage;
-//    @BindView(R.id.list_item_text_view)
-//    TextView mText;
-//    public ViewHolder(View itemView) {
-//        super(itemView);
-//        ButterKnife.bind(this, itemView);
-//    }
-//}
-//
-//    /**
-//     * This callback method inflates the ViewHolder
-//     */
-//    @Override
-//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View listItemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-//        return new ViewHolder(listItemView);
-//    }
-//
-//    /**
-//     * This callback method dynamically binds the ViewHolder to the RecyclerView
-//     */
-//    @Override
-//    public void onBindViewHolder(ViewHolder holder, final int position) {
-//        final Site currentSite = mSites.get(position);
-//        holder.mImage.setImageResource(currentSite.getImageResourceId());
-//        holder.mText.setText(currentSite.getName());
-//        holder.mParentLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Site clickedSite = mSites.get(position);
-//                Log.d(TAG, "onClick:  "+clickedSite);
-//                segueToDetailActivity(clickedSite);
-//            }
-//        });
-//
-//
-//    }
-//
-//    /**
-//     * This callback method is used to determine the number of list items within the RecyclerView
-//     */
-//    @Override
-//    public int getItemCount() {
-//        return mSites.size();
-//    }
-//
-//    /**
-//     * Transfers to the Details Activity along with the Site instance
-//     *  that the user clicked
-//     * @param site is the Site instance being transferred to the Detail Activity
-//     */
-//    private void segueToDetailActivity(Site site){
-//        Intent intent = new Intent(mContext, DetailsActivity.class);
-//        intent.putExtra(Site.INTENT_TAG, site);
-//        mContext.startActivity(intent);
-//    }
-//}
- */
